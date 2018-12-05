500b473cdd06aec3df71c151eb2bfcd6e127ec27
yatharthranjan <yatharthranjan89@gmail.com>
2018-12-05 18:35:52 +0000

    Add Object Mapper Factory to use same instance through out with Autowiring support.

---
+++ src/main/java/org/radarbase/fcm/common/ObjectMapperFactory.java
@@ -0,0 +1,40 @@
+/*
+ *
+ *  *
+ *  *  * Copyright 2018 King's College London
+ *  *  *
+ *  *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  * you may not use this file except in compliance with the License.
+ *  *  * You may obtain a copy of the License at
+ *  *  *
+ *  *  *   http://www.apache.org/licenses/LICENSE-2.0
+ *  *  *
+ *  *  * Unless required by applicable law or agreed to in writing, software
+ *  *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  * See the License for the specific language governing permissions and
+ *  *  * limitations under the License.
+ *  *  *
+ *  *
+ *
+ */
+
+package org.radarbase.fcm.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ObjectMapperFactory extends AbstractFactoryBean<ObjectMapper> {
+
+    @Override
+    public Class<?> getObjectType() {
+        return ObjectMapper.class;
+    }
+
+    @Override
+    protected ObjectMapper createInstance() {
+        return new ObjectMapper();
+    }
+}
