a7eabc3c732627bf358cff6a13d62737fd86755a
mpgxvii <mpgxvii@gmail.com>
2019-11-08 18:04:41 +0000

    fix(quartz listeners): add initial support for data messages in job and scheduler listeners

--- src/main/java/org/radarbase/appserver/event/listener/QuartzNotificationJobListener.java
+++ src/main/java/org/radarbase/appserver/event/listener/QuartzNotificationJobListener.java
@@ -25,99 +25,123 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+
 import lombok.extern.slf4j.Slf4j;
-import org.quartz.JobDetail;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.quartz.JobListener;
-import org.quartz.Scheduler;
-import org.quartz.Trigger;
+import org.quartz.*;
+import org.radarbase.appserver.entity.DataMessage;
+import org.radarbase.appserver.entity.Message;
 import org.radarbase.appserver.entity.Notification;
 import org.radarbase.appserver.event.state.NotificationState;
 import org.radarbase.appserver.event.state.NotificationStateEvent;
+import org.radarbase.appserver.repository.DataMessageRepository;
 import org.radarbase.appserver.repository.NotificationRepository;
+import org.radarbase.appserver.service.MessageType;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@SuppressWarnings("PMD.DataflowAnomalyAnalysis")
 public class QuartzNotificationJobListener implements JobListener {
 
-  private final transient ApplicationEventPublisher notificationStateEventPublisher;
-  private final transient NotificationRepository notificationRepository;
+    private final transient ApplicationEventPublisher notificationStateEventPublisher;
+    private final transient NotificationRepository notificationRepository;
+    private final transient DataMessageRepository dataMessageRepository;
 
-  public QuartzNotificationJobListener(
-      ApplicationEventPublisher notificationStateEventPublisher,
-      NotificationRepository notificationRepository) {
-    this.notificationStateEventPublisher = notificationStateEventPublisher;
-    this.notificationRepository = notificationRepository;
-  }
-
-  /** Get the name of the <code>JobListener</code>. */
-  @Override
-  public String getName() {
-    return getClass().getName();
-  }
-
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> is about to
-   * be executed (an associated <code>{@link Trigger}</code> has occurred).
-   *
-   * <p>This method will not be invoked if the execution of the Job was vetoed by a <code>{
-   * TriggerListener}</code>.
-   *
-   * @see #jobExecutionVetoed(JobExecutionContext)
-   */
-  @Override
-  public void jobToBeExecuted(JobExecutionContext context) {
-    // Not implemented
-  }
-
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> was about to
-   * be executed (an associated <code>{@link Trigger}</code> has occurred), but a <code>{
-   * TriggerListener}</code> vetoed it's execution.
-   *
-   * @see #jobToBeExecuted(JobExecutionContext)
-   */
-  @Override
-  public void jobExecutionVetoed(JobExecutionContext context) {
-    // Not implemented
-  }
-
-  /**
-   * Called by the <code>{@link Scheduler}</code> after a <code>{@link JobDetail}</code> has been
-   * executed, and be for the associated <code>Trigger</code>'s <code>triggered(xx)</code> method
-   * has been called.
-   */
-  @Override
-  public void jobWasExecuted(JobExecutionContext context, JobExecutionException jobException) {
-
-    Optional<Notification> notification =
-        notificationRepository.findById(
-            context.getMergedJobDataMap().getLongValue("notificationId"));
-
-    if (notification.isEmpty()) {
-      log.warn("The notification does not exist in database and yet was scheduled.");
-      return;
+    public QuartzNotificationJobListener(
+            ApplicationEventPublisher notificationStateEventPublisher,
+            NotificationRepository notificationRepository, DataMessageRepository dataMessageRepository) {
+        this.notificationStateEventPublisher = notificationStateEventPublisher;
+        this.notificationRepository = notificationRepository;
+        this.dataMessageRepository = dataMessageRepository;
     }
 
-    if (jobException != null) {
-      Map<String, String> additionalInfo = new HashMap<>();
-      additionalInfo.put("error", jobException.getMessage());
-      additionalInfo.put("error_description", jobException.toString());
-      NotificationStateEvent notificationStateEventError =
-          new NotificationStateEvent(
-              this, notification.get(), NotificationState.ERRORED, additionalInfo, Instant.now());
-      notificationStateEventPublisher.publishEvent(notificationStateEventError);
-
-      log.warn("The job could not be executed.", jobException);
-      return;
+    /**
+     * Get the name of the <code>JobListener</code>.
+     */
+    @Override
+    public String getName() {
+        return getClass().getName();
     }
 
-    NotificationStateEvent notificationStateEvent =
-        new NotificationStateEvent(
-            this, notification.get(), NotificationState.EXECUTED, null, Instant.now());
-    notificationStateEventPublisher.publishEvent(notificationStateEvent);
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> is about to
+     * be executed (an associated <code>{@link Trigger}</code> has occurred).
+     *
+     * <p>This method will not be invoked if the execution of the Job was vetoed by a <code>{
+     * TriggerListener}</code>.
+     *
+     * @see #jobExecutionVetoed(JobExecutionContext)
+     */
+    @Override
+    public void jobToBeExecuted(JobExecutionContext context) {
+        // Not implemented
+    }
+
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> was about to
+     * be executed (an associated <code>{@link Trigger}</code> has occurred), but a <code>{
+     * TriggerListener}</code> vetoed it's execution.
+     *
+     * @see #jobToBeExecuted(JobExecutionContext)
+     */
+    @Override
+    public void jobExecutionVetoed(JobExecutionContext context) {
+        // Not implemented
+    }
+
+    /**
+     * Called by the <code>{@link Scheduler}</code> after a <code>{@link JobDetail}</code> has been
+     * executed, and be for the associated <code>Trigger</code>'s <code>triggered(xx)</code> method
+     * has been called.
+     */
+    @Override
+    public void jobWasExecuted(JobExecutionContext context, JobExecutionException jobException) {
+        JobDataMap jobDataMap = context.getMergedJobDataMap();
+        String type = jobDataMap.getString("messageType");
+        Long messageId = jobDataMap.getLongValue("messageId");
+
+        Message message = new Message();
+
+        if (type.equals(MessageType.Notification.toString())) {
+            Optional<Notification> notification =
+                    notificationRepository.findById(messageId);
+            if (notification.isEmpty()) {
+                log.warn("The notification does not exist in database and yet was scheduled.");
+                return;
+            }
+            message = notification.get();
+
+            if (jobException != null) {
+                Map<String, String> additionalInfo = new HashMap<>();
+                additionalInfo.put("error", jobException.getMessage());
+                additionalInfo.put("error_description", jobException.toString());
+                NotificationStateEvent notificationStateEventError =
+                        new NotificationStateEvent(
+                                this, notification.get(), NotificationState.ERRORED, additionalInfo, Instant.now());
+                notificationStateEventPublisher.publishEvent(notificationStateEventError);
+
+                log.warn("The job could not be executed.", jobException);
+                return;
+            }
+
+            NotificationStateEvent notificationStateEvent =
+                    new NotificationStateEvent(
+                            this, notification.get(), NotificationState.EXECUTED, null, Instant.now());
+            notificationStateEventPublisher.publishEvent(notificationStateEvent);
+
+        }
+        // TODO: ADD SUPPORT FOR DATA MESSAGES
+        if (type.equals(MessageType.Data.toString())) {
+            Optional<DataMessage> dataMessage =
+                    dataMessageRepository.findById(messageId);
+            if (dataMessage.isEmpty()) {
+                log.warn("The data message does not exist in database and yet was scheduled.");
+                return;
+            }
+            message = dataMessage.get();
+        }
+
+
+    }
 }
--- src/main/java/org/radarbase/appserver/event/listener/QuartzNotificationSchedulerListener.java
+++ src/main/java/org/radarbase/appserver/event/listener/QuartzNotificationSchedulerListener.java
@@ -23,18 +23,14 @@ package org.radarbase.appserver.event.listener;
 
 import java.time.Instant;
 import java.util.Optional;
+
 import lombok.extern.slf4j.Slf4j;
-import org.quartz.JobDetail;
-import org.quartz.JobKey;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.SchedulerListener;
-import org.quartz.Trigger;
-import org.quartz.TriggerKey;
+import org.quartz.*;
 import org.radarbase.appserver.entity.Notification;
 import org.radarbase.appserver.event.state.NotificationState;
 import org.radarbase.appserver.event.state.NotificationStateEvent;
 import org.radarbase.appserver.repository.NotificationRepository;
+import org.radarbase.appserver.service.MessageType;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
@@ -43,235 +39,249 @@ import org.springframework.stereotype.Component;
 @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
 public class QuartzNotificationSchedulerListener implements SchedulerListener {
 
-  private final transient ApplicationEventPublisher notificationStateEventPublisher;
-  private final transient NotificationRepository notificationRepository;
-  private final transient Scheduler scheduler;
+    private final transient ApplicationEventPublisher notificationStateEventPublisher;
+    private final transient NotificationRepository notificationRepository;
+    private final transient Scheduler scheduler;
 
-  public QuartzNotificationSchedulerListener(
-      ApplicationEventPublisher notificationStateEventPublisher,
-      NotificationRepository notificationRepository,
-      Scheduler scheduler) {
-    this.notificationStateEventPublisher = notificationStateEventPublisher;
-    this.notificationRepository = notificationRepository;
-    this.scheduler = scheduler;
-  }
-
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> is
-   * scheduled.
-   */
-  @Override
-  public void jobScheduled(Trigger trigger) {
-    JobDetail jobDetail;
-    try {
-      jobDetail = scheduler.getJobDetail(trigger.getJobKey());
-    } catch (SchedulerException exc) {
-      log.warn("Encountered error while getting job information from Trigger: ", exc);
-      return;
+    public QuartzNotificationSchedulerListener(
+            ApplicationEventPublisher notificationStateEventPublisher,
+            NotificationRepository notificationRepository,
+            Scheduler scheduler) {
+        this.notificationStateEventPublisher = notificationStateEventPublisher;
+        this.notificationRepository = notificationRepository;
+        this.scheduler = scheduler;
     }
 
-    Optional<Notification> notification =
-        notificationRepository.findById(jobDetail.getJobDataMap().getLongValue("notificationId"));
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> is
+     * scheduled.
+     */
+    @Override
+    public void jobScheduled(Trigger trigger) {
+        JobDetail jobDetail;
+        try {
+            jobDetail = scheduler.getJobDetail(trigger.getJobKey());
+        } catch (SchedulerException exc) {
+            log.warn("Encountered error while getting job information from Trigger: ", exc);
+            return;
+        }
 
-    if (notification.isEmpty()) {
-      log.warn("The notification does not exist in database and yet was scheduled.");
-      return;
+        JobDataMap jobDataMap = jobDetail.getJobDataMap();
+        String type = jobDataMap.getString("messageType");
+        Long messageId = jobDataMap.getLongValue("messageId");
+
+        Optional<Notification> notification =
+                notificationRepository.findById(messageId);
+        if (type.equals(MessageType.Notification.toString())) {
+
+            if (notification.isEmpty()) {
+                log.warn("The notification does not exist in database and yet was scheduled.");
+                return;
+            }
+            NotificationStateEvent notificationStateEvent =
+                    new NotificationStateEvent(
+                            this, notification.get(), NotificationState.SCHEDULED, null, Instant.now());
+            notificationStateEventPublisher.publishEvent(notificationStateEvent);
+        }
+
+        // TODO: ADD SUPPORT FOR DATA MESSAGES
     }
-    NotificationStateEvent notificationStateEvent =
-        new NotificationStateEvent(
-            this, notification.get(), NotificationState.SCHEDULED, null, Instant.now());
-    notificationStateEventPublisher.publishEvent(notificationStateEvent);
-  }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> is
-   * unscheduled.
-   *
-   * @see SchedulerListener#schedulingDataCleared()
-   */
-  @Override
-  public void jobUnscheduled(TriggerKey triggerKey) {
-    JobDetail jobDetail;
-    try {
-      jobDetail = scheduler.getJobDetail(scheduler.getTrigger(triggerKey).getJobKey());
-    } catch (SchedulerException exc) {
-      log.warn("Encountered error while getting job information from Trigger: ", exc);
-      return;
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> is
+     * unscheduled.
+     *
+     * @see SchedulerListener#schedulingDataCleared()
+     */
+    @Override
+    public void jobUnscheduled(TriggerKey triggerKey) {
+        JobDetail jobDetail;
+        try {
+            jobDetail = scheduler.getJobDetail(scheduler.getTrigger(triggerKey).getJobKey());
+        } catch (SchedulerException exc) {
+            log.warn("Encountered error while getting job information from Trigger: ", exc);
+            return;
+        }
+
+        Optional<Notification> notification =
+                notificationRepository.findById(jobDetail.getJobDataMap().getLongValue("notificationId"));
+
+        if (notification.isEmpty()) {
+            log.warn("The notification does not exist in database and yet was unscheduled.");
+            return;
+        }
+        NotificationStateEvent notificationStateEvent =
+                new NotificationStateEvent(
+                        this, notification.get(), NotificationState.CANCELLED, null, Instant.now());
+        notificationStateEventPublisher.publishEvent(notificationStateEvent);
     }
 
-    Optional<Notification> notification =
-        notificationRepository.findById(jobDetail.getJobDataMap().getLongValue("notificationId"));
-
-    if (notification.isEmpty()) {
-      log.warn("The notification does not exist in database and yet was unscheduled.");
-      return;
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a <code>{@link Trigger}</code> has reached
+     * the condition in which it will never fire again.
+     */
+    @Override
+    public void triggerFinalized(Trigger trigger) {
+        // Not implemented
     }
-    NotificationStateEvent notificationStateEvent =
-        new NotificationStateEvent(
-            this, notification.get(), NotificationState.CANCELLED, null, Instant.now());
-    notificationStateEventPublisher.publishEvent(notificationStateEvent);
-  }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a <code>{@link Trigger}</code> has reached
-   * the condition in which it will never fire again.
-   */
-  @Override
-  public void triggerFinalized(Trigger trigger) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a <code>{@link Trigger}</code> has been
+     * paused.
+     */
+    @Override
+    public void triggerPaused(TriggerKey triggerKey) {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a <code>{@link Trigger}</code> has been
-   * paused.
-   */
-  @Override
-  public void triggerPaused(TriggerKey triggerKey) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a group of <code>{@link Trigger}s</code> has
+     * been paused.
+     *
+     * <p>If all groups were paused then triggerGroup will be null
+     *
+     * @param triggerGroup the paused group, or null if all were paused
+     */
+    @Override
+    public void triggersPaused(String triggerGroup) {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a group of <code>{@link Trigger}s</code> has
-   * been paused.
-   *
-   * <p>If all groups were paused then triggerGroup will be null
-   *
-   * @param triggerGroup the paused group, or null if all were paused
-   */
-  @Override
-  public void triggersPaused(String triggerGroup) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a <code>{@link Trigger}</code> has been
+     * un-paused.
+     */
+    @Override
+    public void triggerResumed(TriggerKey triggerKey) {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a <code>{@link Trigger}</code> has been
-   * un-paused.
-   */
-  @Override
-  public void triggerResumed(TriggerKey triggerKey) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a group of <code>{@link Trigger}s</code> has
+     * been un-paused.
+     */
+    @Override
+    public void triggersResumed(String triggerGroup) {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a group of <code>{@link Trigger}s</code> has
-   * been un-paused.
-   */
-  @Override
-  public void triggersResumed(String triggerGroup) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> has been
+     * added.
+     */
+    @Override
+    public void jobAdded(JobDetail jobDetail) {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> has been
-   * added.
-   */
-  @Override
-  public void jobAdded(JobDetail jobDetail) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> has been
+     * deleted.
+     */
+    @Override
+    public void jobDeleted(JobKey jobKey) {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> has been
-   * deleted.
-   */
-  @Override
-  public void jobDeleted(JobKey jobKey) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> has been
+     * paused.
+     */
+    @Override
+    public void jobPaused(JobKey jobKey) {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> has been
-   * paused.
-   */
-  @Override
-  public void jobPaused(JobKey jobKey) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a group of <code>{@link JobDetail}s</code>
+     * has been paused.
+     *
+     * @param jobGroup the paused group, or null if all were paused
+     */
+    @Override
+    public void jobsPaused(String jobGroup) {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a group of <code>{@link JobDetail}s</code>
-   * has been paused.
-   *
-   * @param jobGroup the paused group, or null if all were paused
-   */
-  @Override
-  public void jobsPaused(String jobGroup) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> has been
+     * un-paused.
+     */
+    @Override
+    public void jobResumed(JobKey jobKey) {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a <code>{@link JobDetail}</code> has been
-   * un-paused.
-   */
-  @Override
-  public void jobResumed(JobKey jobKey) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a group of <code>{@link JobDetail}s</code>
+     * has been un-paused.
+     */
+    @Override
+    public void jobsResumed(String jobGroup) {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a group of <code>{@link JobDetail}s</code>
-   * has been un-paused.
-   */
-  @Override
-  public void jobsResumed(String jobGroup) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> when a serious error has occurred within the
+     * scheduler - such as repeated failures in the <code>JobStore</code>, or the inability to
+     * instantiate a <code>Job</code> instance when its <code>Trigger</code> has fired.
+     *
+     * <p>The <code>getErrorCode()</code> method of the given SchedulerException can be used to
+     * determine more specific information about the type of error that was encountered.
+     */
+    @Override
+    public void schedulerError(String msg, SchedulerException cause) {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> when a serious error has occurred within the
-   * scheduler - such as repeated failures in the <code>JobStore</code>, or the inability to
-   * instantiate a <code>Job</code> instance when its <code>Trigger</code> has fired.
-   *
-   * <p>The <code>getErrorCode()</code> method of the given SchedulerException can be used to
-   * determine more specific information about the type of error that was encountered.
-   */
-  @Override
-  public void schedulerError(String msg, SchedulerException cause) {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> to inform the listener that it has move to standby
+     * mode.
+     */
+    @Override
+    public void schedulerInStandbyMode() {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> to inform the listener that it has move to standby
-   * mode.
-   */
-  @Override
-  public void schedulerInStandbyMode() {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> to inform the listener that it has started.
+     */
+    @Override
+    public void schedulerStarted() {
+        // Not implemented
+    }
 
-  /** Called by the <code>{@link Scheduler}</code> to inform the listener that it has started. */
-  @Override
-  public void schedulerStarted() {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> to inform the listener that it is starting.
+     */
+    @Override
+    public void schedulerStarting() {
+        // Not implemented
+    }
 
-  /** Called by the <code>{@link Scheduler}</code> to inform the listener that it is starting. */
-  @Override
-  public void schedulerStarting() {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> to inform the listener that it has shutdown.
+     */
+    @Override
+    public void schedulerShutdown() {
+        // Not implemented
+    }
 
-  /** Called by the <code>{@link Scheduler}</code> to inform the listener that it has shutdown. */
-  @Override
-  public void schedulerShutdown() {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> to inform the listener that it has begun the
+     * shutdown sequence.
+     */
+    @Override
+    public void schedulerShuttingdown() {
+        // Not implemented
+    }
 
-  /**
-   * Called by the <code>{@link Scheduler}</code> to inform the listener that it has begun the
-   * shutdown sequence.
-   */
-  @Override
-  public void schedulerShuttingdown() {
-    // Not implemented
-  }
-
-  /**
-   * Called by the <code>{@link Scheduler}</code> to inform the listener that all jobs, triggers and
-   * calendars were deleted.
-   */
-  @Override
-  public void schedulingDataCleared() {
-    // Not implemented
-  }
+    /**
+     * Called by the <code>{@link Scheduler}</code> to inform the listener that all jobs, triggers and
+     * calendars were deleted.
+     */
+    @Override
+    public void schedulingDataCleared() {
+        // Not implemented
+    }
 }
