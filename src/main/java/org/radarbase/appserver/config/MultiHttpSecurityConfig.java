a05a39013349086667d158330e79fbf24b59e316
mpgxvii <mpgxvii@gmail.com>
2020-03-20 20:55:45 +0000

    fix: cors configurer

--- src/main/java/org/radarbase/appserver/config/MultiHttpSecurityConfig.java
+++ src/main/java/org/radarbase/appserver/config/MultiHttpSecurityConfig.java
@@ -132,36 +132,4 @@ public class MultiHttpSecurityConfig {
         };
     }
 
-    // @Component
-    // @Order(Ordered.HIGHEST_PRECEDENCE)
-    // public static class SimpleCorsFilter implements Filter {
-
-    //     public SimpleCorsFilter() {
-    //     }
-
-    //     @Override
-    //     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-    //         HttpServletResponse response = (HttpServletResponse) res;
-    //         HttpServletRequest request = (HttpServletRequest) req;
-    //         response.setHeader("Access-Control-Allow-Origin", "*");
-    //         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-    //         response.setHeader("Access-Control-Max-Age", "3600");
-    //         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type");
-
-    //         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-    //             response.setStatus(HttpServletResponse.SC_OK);
-    //         } else {
-    //             chain.doFilter(req, res);
-    //         }
-    //     }
-
-    //     @Override
-    //     public void init(FilterConfig filterConfig) {
-    //     }
-
-    //     @Override
-    //     public void destroy() {
-    //     }
-    // }
-
 }
