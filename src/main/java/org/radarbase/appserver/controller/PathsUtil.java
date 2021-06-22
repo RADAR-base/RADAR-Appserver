cd91cdee6b08d87fd70da7cc869ce79014566ba6
mpgxvii <mpgxvii@gmail.com>
2021-06-22 16:52:29 +0100

    Update delete notifications endpoint path

--- src/main/java/org/radarbase/appserver/controller/PathsUtil.java
+++ src/main/java/org/radarbase/appserver/controller/PathsUtil.java
@@ -28,13 +28,15 @@ package org.radarbase.appserver.controller;
  */
 public class PathsUtil {
 
-  public static final String USER_PATH = "users";
-  public static final String PROJECT_PATH = "projects";
-  public static final String MESSAGING_NOTIFICATION_PATH = "messaging/notifications";
-  public static final String MESSAGING_DATA_PATH = "messaging/data";
-  public static final String PROTOCOL_PATH = "protocols";
-  public static final String PROJECT_ID_CONSTANT = "{projectId}";
-  public static final String SUBJECT_ID_CONSTANT = "{subjectId}";
-  public static final String NOTIFICATION_ID_CONSTANT = "{notificationId}";
-  public static final String NOTIFICATION_STATE_EVENTS_PATH = "state_events";
+    public static final String USER_PATH = "users";
+    public static final String PROJECT_PATH = "projects";
+    public static final String MESSAGING_NOTIFICATION_PATH = "messaging/notifications";
+    public static final String MESSAGING_DATA_PATH = "messaging/data";
+    public static final String PROTOCOL_PATH = "protocols";
+    public static final String PROJECT_ID_CONSTANT = "{projectId}";
+    public static final String SUBJECT_ID_CONSTANT = "{subjectId}";
+    public static final String NOTIFICATION_ID_CONSTANT = "{notificationId}";
+    public static final String NOTIFICATION_STATE_EVENTS_PATH = "state_events";
+    public static final String ALL_KEYWORD = "all";
+
 }
