38954ff868e9b9ec8aaae8bb1678e61a199f04cb
mpgxvii <mpgxvii@gmail.com>
2022-06-07 16:55:15 +0100

    fix: protocolGenerator test

--- src/test/java/org/radarbase/appserver/service/questionnaire/protocol/DefaultProtocolGeneratorTest.java
+++ src/test/java/org/radarbase/appserver/service/questionnaire/protocol/DefaultProtocolGeneratorTest.java
@@ -30,7 +30,7 @@ import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.radarbase.appserver.dto.protocol.Assessment;
 import org.radarbase.appserver.dto.protocol.AssessmentProtocol;
 import org.radarbase.appserver.dto.protocol.LanguageText;
@@ -41,9 +41,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(
         webEnvironment = WebEnvironment.NONE,
         classes = {GithubProtocolFetcherStrategy.class, DefaultProtocolGenerator.class})
