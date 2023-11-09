b9069d55a4daab200a59593feb8a6a7ad7fa29ce
Pauline <mpgxvii@gmail.com>
2023-11-09 12:48:19 +0000

    Update auth to be compatible with latest version of radar-auth and radar-spring

--- src/main/java/org/radarbase/appserver/config/AuthConfig.java
+++ src/main/java/org/radarbase/appserver/config/AuthConfig.java
@@ -1,9 +1,6 @@
 package org.radarbase.appserver.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.radarbase.auth.config.TokenValidatorConfig;
-import org.radarbase.auth.config.TokenVerifierPublicKeyConfig;
-import org.radarbase.auth.exception.ConfigurationException;
 import org.radarbase.auth.token.RadarToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -45,15 +42,7 @@ public class AuthConfig {
   @Bean
   AuthValidator<RadarToken> getAuthValidator(
       @Autowired ManagementPortalAuthProperties managementPortalAuthProperties) {
-    try {
-      TokenValidatorConfig validatorConfig = TokenVerifierPublicKeyConfig.readFromFileOrClasspath();
-      return new ManagementPortalAuthValidator(managementPortalAuthProperties, validatorConfig);
-    } catch (ConfigurationException exc) {
-      log.warn(
-          "Could not loading config from RADAR_IS file. Now using default public key "
-              + "endpoint...");
-      return new ManagementPortalAuthValidator(managementPortalAuthProperties);
-    }
+    return new ManagementPortalAuthValidator(managementPortalAuthProperties);
   }
 
   @Bean
