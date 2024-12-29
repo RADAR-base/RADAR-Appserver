4cbef07f4db290b5ab4f063a4664a24d628941d2
this-Aditya <adityamishra2217@gmail.com>
2024-12-29 21:17:53 +0530

    Refactored project service and repository

---
+++ src/main/java/org/radarbase/appserver/repository/ProjectRepository.kt
@@ -0,0 +1,11 @@
+package org.radarbase.appserver.repository
+
+import org.radarbase.appserver.entity.Project
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProjectRepository : JpaRepository<Project, Long> {
+    fun findByProjectId(projectId: String): Project?
+    fun existsByProjectId(projectId: String): Boolean
+}
