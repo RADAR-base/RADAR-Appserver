8b37f2f07b6b4cb14bc95eb22a4725d3f8b4e4fb
this-Aditya <adityamishra2217@gmail.com>
2024-12-31 00:57:04 +0530

    Updated Project entity and auditing class

---
+++ src/main/java/org/radarbase/appserver/entity/Project.kt
@@ -0,0 +1,65 @@
+package org.radarbase.appserver.entity
+
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotNull
+import org.radarbase.appserver.util.equalTo
+import org.radarbase.appserver.util.stringRepresentation
+import java.io.Serial
+import java.io.Serializable
+import java.util.*
+
+/**
+ * Represents an entity for the "projects" table in the database.
+ *
+ * @property id The primary key of the project, which is generated automatically.
+ * @property projectId A unique identifier for the project. This field is mandatory.
+ */
+@Entity
+@Table(name = "projects")
+class Project(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long? = null,
+
+    @field:NotNull
+    @Column(name = "project_id", unique = true, nullable = false)
+    val projectId: String
+) : Serializable, AuditModel() {
+
+    constructor(projectId: String) : this(null, projectId)
+
+    companion object {
+        @Serial
+        private const val serialVersionUID = 12312466855464L
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return equalTo(
+            other,
+            Project::projectId,
+        )
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(projectId)
+    }
+
+    override fun toString(): String = stringRepresentation(
+        Project::id,
+        Project::projectId,
+        Project::createdAt,
+        Project::updatedAt
+    )
+
+    /**
+     * Creates a copy of the current Project object with the specified properties.
+     *
+     * @param id Optional id to override the current project's id. Defaults to the current id.
+     * @param projectId The project identifier to set for the new Project object. Defaults to the current projectId.
+     * @return A new Project object with the specified or inherited properties.
+     */
+    fun copy(
+        id: Long? = this.id,
+        projectId: String = this.projectId,
+    ): Project = Project(id, projectId)
+}
