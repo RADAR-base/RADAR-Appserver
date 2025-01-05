e48ce27dda9d2ee637fbee274d66a23cd2232e1b
this-Aditya <adityamishra2217@gmail.com>
2025-01-06 04:31:21 +0530

    Using mockito again instead of mockk for now

--- src/test/java/org/radarbase/appserver/service/UserServiceTest.kt
+++ src/test/java/org/radarbase/appserver/service/UserServiceTest.kt
@@ -1,34 +1,10 @@
-/*
- *
- *  *
- *  *  * Copyright 2018 King's College London
- *  *  *
- *  *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  *  * you may not use this file except in compliance with the License.
- *  *  * You may obtain a copy of the License at
- *  *  *
- *  *  *   http://www.apache.org/licenses/LICENSE-2.0
- *  *  *
- *  *  * Unless required by applicable law or agreed to in writing, software
- *  *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  *  * See the License for the specific language governing permissions and
- *  *  * limitations under the License.
- *  *  *
- *  *
- *
- */
 package org.radarbase.appserver.service
 
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito
-import org.mockito.invocation.InvocationOnMock
-import org.mockito.stubbing.Answer
-import org.radarbase.appserver.controller.FcmNotificationControllerTest
-import org.radarbase.appserver.controller.RadarUserControllerTest
 import org.radarbase.appserver.dto.fcm.FcmUserDto
 import org.radarbase.appserver.entity.Project
 import org.radarbase.appserver.entity.User
@@ -45,272 +21,177 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.time.Duration
 import java.time.Instant
 import java.util.*
-import java.util.List
 
 @ExtendWith(SpringExtension::class)
 @DataJpaTest
-internal class UserServiceTest {
+class UserServiceTest {
+
     @Autowired
-    @Transient
-    private val userService: UserService? = null
+    private lateinit var userService: UserService
 
     @MockBean
-    @Transient
-    private val userRepository: UserRepository? = null
+    private lateinit var userRepository: UserRepository
 
     @MockBean
-    @Transient
-    private val projectRepository: ProjectRepository? = null
+    private lateinit var projectRepository: ProjectRepository
 
     @MockBean
-    @Transient
-    private val scheduleService: QuestionnaireScheduleService? = null
+    private lateinit var scheduleService: QuestionnaireScheduleService
 
-    @Transient
-    private val enrolmentDate: Instant? = Instant.now().plus(Duration.ofSeconds(100))
+    private val enrolmentDate: Instant = Instant.now().plus(Duration.ofSeconds(100))
 
     @BeforeEach
     fun setUp() {
-        // given
-        val project = Project(1L, FcmNotificationControllerTest.PROJECT_ID)
 
-        Mockito.`when`<Project?>(projectRepository!!.findByProjectId(project.projectId))
-            .thenReturn(project)
+        val project = Project().apply {
+            projectId = PROJECT_ID
+            id = 1L
+        }
 
-        val user = User(
-            1L,
-            FcmNotificationControllerTest.USER_ID,
-            null,
-            RadarUserControllerTest.FCM_TOKEN_1,
-            project,
-            enrolmentDate,
-            null,
-            TIMEZONE,
-            "en",
-            null
-        )
+        Mockito.`when`(projectRepository.findByProjectId(project.projectId)).thenReturn(project)
 
-        Mockito.`when`<MutableList<User>?>(userRepository!!.findAll()).thenReturn(List.of<User>(user))
+        val user = User().apply {
+            fcmToken = FCM_TOKEN_1
+            enrolmentDate = this@UserServiceTest.enrolmentDate
+            this.project = project
+            timezone = TIMEZONE
+            language = "en"
+            subjectId = USER_ID
+            id = 1L
+        }
 
-        Mockito.`when`<Optional<User>?>(userRepository.findById(1L)).thenReturn(Optional.of<User>(user))
+        val userNew = User().apply {
+            subjectId = "$USER_ID-2"
+            fcmToken = FCM_TOKEN_1
+            this.project = project
+            enrolmentDate = this@UserServiceTest.enrolmentDate
+            language = "es"
+            timezone = TIMEZONE
+        }
 
-        Mockito.`when`<User?>(userRepository.findBySubjectId(user.subjectId)).thenReturn(user)
+        val userUpdated = User().apply {
+            subjectId = USER_ID
+            fcmToken = "xxxxyyy"
+            this.project = project
+            enrolmentDate = this@UserServiceTest.enrolmentDate
+            language = "es"
+            timezone = TIMEZONE
+            usermetrics = UserMetrics().apply {
+                lastDelivered = this@UserServiceTest.enrolmentDate
+                lastOpened = this@UserServiceTest.enrolmentDate
+            }
+        }
 
-        Mockito.`when`<User?>(userRepository.findBySubjectIdAndProjectId(FcmNotificationControllerTest.USER_ID, 1L))
-            .thenReturn(user)
-
-        Mockito.`when`<MutableList<User>?>(userRepository.findByProjectId(1L)).thenReturn(List.of<User>(user))
-
-        val newUser = User(
-            null,
-            FcmNotificationControllerTest.USER_ID + "-2",
-            null,
-            RadarUserControllerTest.FCM_TOKEN_1,
-            project,
-            enrolmentDate,
-            null,
-            TIMEZONE,
-            "es",
-            null
-        )
-
-        val userMetrics2 = UserMetrics(null, null, null, newUser)
-        newUser.usermetrics = userMetrics2
-
-        val newUserWithId = User(
-            2L,
-            FcmNotificationControllerTest.USER_ID + "-2",
-            null,
-            RadarUserControllerTest.FCM_TOKEN_1,
-            project,
-            enrolmentDate,
-            null,
-            TIMEZONE,
-            "es",
-            null
-        )
-
-        Mockito.`when`<User?>(userRepository.saveAndFlush<User?>(newUser))
-            .thenAnswer(Answer { invocation: InvocationOnMock? ->
-                val userN = invocation!!.getArgument<User>(0)
-                println("CheckPoint UserN: " + userN)
-                User(
-                    2L,
-                    userN.subjectId,
-                    userN.emailAddress,
-                    userN.fcmToken,
-                    userN.project,
-                    userN.enrolmentDate,
-                    userN.usermetrics,
-                    userN.timezone,
-                    userN.language,
-                    userN.attributes
-                )
-            })
-
-
-        val userMetrics = UserMetrics(enrolmentDate, enrolmentDate)
-
-        val updatedUser = User(
-            null,
-            FcmNotificationControllerTest.USER_ID,
-            null,
-            "xxxxyyy",
-            project,
-            enrolmentDate,
-            userMetrics,
-            TIMEZONE,
-            "es",
-            null
-        )
-        val updatedUserWithId = User(
-            1L,
-            FcmNotificationControllerTest.USER_ID,
-            null,
-            "xxxxyyy",
-            project,
-            enrolmentDate,
-            userMetrics,
-            TIMEZONE,
-            "es",
-            null
-        )
-        Mockito.`when`<User?>(userRepository.saveAndFlush<User?>(updatedUser)).thenReturn(updatedUserWithId)
+        Mockito.`when`(userRepository.findAll()).thenReturn(listOf(user))
+        Mockito.`when`(userRepository.findById(1L)).thenReturn(Optional.of(user))
+        Mockito.`when`(userRepository.findBySubjectId(user.subjectId)).thenReturn(user)
+        Mockito.`when`(userRepository.findBySubjectIdAndProjectId(USER_ID, 1L)).thenReturn(user)
+        Mockito.`when`(userRepository.findByProjectId(1L)).thenReturn(listOf(user))
+        Mockito.`when`(userRepository.saveAndFlush(userNew)).thenReturn(userNew.apply { id = 2L })
+        Mockito.`when`(userRepository.saveAndFlush(userUpdated)).thenReturn(userUpdated.apply { id = 1L })
     }
 
-    @get:Test
-    val allRadarUsers: Unit
-        get() {
-            val users = userService!!.getAllRadarUsers()
+    @Test
+    fun getAllRadarUsers() {
+        val users = userService.getAllRadarUsers()
+        assertEquals(USER_ID, users.users[0].subjectId)
+        assertEquals("en", users.users[0].language)
+        assertEquals(PROJECT_ID, users.users[0].projectId)
+    }
 
-            Assertions.assertEquals(
-                FcmNotificationControllerTest.USER_ID,
-                users.users.get(0).subjectId
-            )
-            Assertions.assertEquals("en", users.users.get(0).language, "en")
-            Assertions.assertEquals(
-                FcmNotificationControllerTest.PROJECT_ID,
-                users.users.get(0).projectId
-            )
-        }
+    @Test
+    fun getUserById() {
+        val userDto = userService.getUserById(1L)
+        assertEquals(USER_ID, userDto.subjectId)
+        assertEquals("en", userDto.language)
+        assertEquals(PROJECT_ID, userDto.projectId)
+    }
 
-    @get:Test
-    val userById: Unit
-        get() {
-            val userDto = userService!!.getUserById(1L)
+    @Test
+    fun getUserBySubjectId() {
+        val userDto = userService.getUserBySubjectId(USER_ID)
+        assertEquals(USER_ID, userDto.subjectId)
+        assertEquals("en", userDto.language)
+        assertEquals(PROJECT_ID, userDto.projectId)
+        assertEquals(1L, userDto.id)
+    }
 
-            Assertions.assertEquals(FcmNotificationControllerTest.USER_ID, userDto.subjectId)
-            Assertions.assertEquals("en", userDto.language)
-            Assertions.assertEquals(FcmNotificationControllerTest.PROJECT_ID, userDto.projectId)
-        }
-
-    @get:Test
-    val userBySubjectId: Unit
-        get() {
-            val userDto = userService!!.getUserBySubjectId(FcmNotificationControllerTest.USER_ID)
-
-            Assertions.assertEquals(FcmNotificationControllerTest.USER_ID, userDto.subjectId)
-            Assertions.assertEquals("en", userDto.language)
-            Assertions.assertEquals(FcmNotificationControllerTest.PROJECT_ID, userDto.projectId)
-            Assertions.assertEquals(1L, userDto.id)
-        }
-
-    @get:Test
-    val usersByProjectId: Unit
-        get() {
-            val users = userService!!.getUsersByProjectId(FcmNotificationControllerTest.PROJECT_ID)
-
-            Assertions.assertEquals(
-                FcmNotificationControllerTest.USER_ID,
-                users.users.get(0).subjectId
-            )
-            Assertions.assertEquals("en", users.users.get(0).language, "en")
-            Assertions.assertEquals(
-                FcmNotificationControllerTest.PROJECT_ID,
-                users.users.get(0).projectId
-            )
-        }
+    @Test
+    fun getUsersByProjectId() {
+        val users = userService.getUsersByProjectId(PROJECT_ID)
+        assertEquals(USER_ID, users.users[0].subjectId)
+        assertEquals("en", users.users[0].language)
+        assertEquals(PROJECT_ID, users.users[0].projectId)
+    }
 
     @Test
     fun saveUserInProject() {
-        val userDtoNew =
-            FcmUserDto(
-                null,
-                FcmNotificationControllerTest.PROJECT_ID,
-                FcmNotificationControllerTest.USER_ID + "-2",
-                null,
-                null,
-                null,
-                null,
-                null,
-                enrolmentDate,
-                TIMEZONE,
-                RadarUserControllerTest.FCM_TOKEN_1,
-                "es",
-                null
-            )
+        val userDtoNew = FcmUserDto().apply {
+            subjectId = "$USER_ID-2"
+            fcmToken = FCM_TOKEN_1
+            projectId = PROJECT_ID
+            enrolmentDate = this@UserServiceTest.enrolmentDate
+            language = "es"
+            timezone = TIMEZONE
+        }
 
-        val userDto = userService!!.saveUserInProject(userDtoNew)
 
-        Assertions.assertEquals(FcmNotificationControllerTest.USER_ID + "-2", userDto.subjectId)
-        Assertions.assertEquals("es", userDto.language)
-        Assertions.assertEquals(FcmNotificationControllerTest.PROJECT_ID, userDto.projectId)
-        Assertions.assertEquals(2L, userDto.id)
+        val userDto = userService.saveUserInProject(userDtoNew)
+
+        assertEquals("$USER_ID-2", userDto.subjectId)
+        assertEquals("es", userDto.language)
+        assertEquals(PROJECT_ID, userDto.projectId)
+        assertEquals(2L, userDto.id)
     }
 
     @Test
     fun updateUser() {
-        val userDtoNew =
-            FcmUserDto(
-                null,
-                FcmNotificationControllerTest.PROJECT_ID,
-                FcmNotificationControllerTest.USER_ID,
-                null,
-                enrolmentDate,
-                enrolmentDate,
-                null,
-                null,
-                enrolmentDate,
-                "Europe/Bucharest",
-                "xxxxyyy",
-                "es",
-                HashMap<String?, String?>()
-            )
+        val userDtoNew = FcmUserDto().apply {
+            subjectId = USER_ID
+            fcmToken = "xxxxyyy"
+            projectId = PROJECT_ID
+            enrolmentDate = this@UserServiceTest.enrolmentDate
+            language = "es"
+            lastDelivered = this@UserServiceTest.enrolmentDate
+            lastOpened = this@UserServiceTest.enrolmentDate
+            attributes = HashMap()
+            timezone = "Europe/Bucharest"
+        }
 
-        val userDto = userService!!.updateUser(userDtoNew)
+        val userDto = userService.updateUser(userDtoNew)
 
-        Assertions.assertEquals(FcmNotificationControllerTest.USER_ID, userDto.subjectId)
-        Assertions.assertEquals("es", userDto.language)
-        Assertions.assertEquals(FcmNotificationControllerTest.PROJECT_ID, userDto.projectId)
-        Assertions.assertEquals("Europe/Bucharest", userDto.timezone)
-        Assertions.assertEquals("xxxxyyy", userDto.fcmToken)
-        Assertions.assertEquals(1L, userDto.id)
+        assertEquals(USER_ID, userDto.subjectId)
+        assertEquals("es", userDto.language)
+        assertEquals(PROJECT_ID, userDto.projectId)
+        assertEquals("Europe/Bucharest", userDto.timezone)
+        assertEquals("xxxxyyy", userDto.fcmToken)
+        assertEquals(1L, userDto.id)
     }
 
     @TestConfiguration
-    internal class UserServiceConfig {
-        @Autowired
-        @Transient
-        private val userRepository: UserRepository? = null
+    class UserServiceConfig {
 
         @Autowired
-        @Transient
-        private val projectRepository: ProjectRepository? = null
+        private lateinit var userRepository: UserRepository
 
         @Autowired
-        @Transient
-        private val scheduleService: QuestionnaireScheduleService? = null
+        private lateinit var projectRepository: ProjectRepository
 
-        @Transient
+        @Autowired
+        private lateinit var scheduleService: QuestionnaireScheduleService
+
         private val userMapper = UserMapper()
 
         @Bean
         fun userServiceBeanConfig(): UserService {
-            return UserService(false, userMapper, userRepository!!, projectRepository!!, scheduleService!!)
+            return UserService(false, userMapper, userRepository, projectRepository, scheduleService)
         }
     }
 
     companion object {
         private const val TIMEZONE = "Europe/Bucharest"
+        private const val USER_ID = "test-user"
+        private const val PROJECT_ID = "test-project"
+        private const val FCM_TOKEN_1 = "xxxx";
     }
 }
