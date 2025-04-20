e2612f92f7084dc3894ebd74d09fe214069c76d4
this-Aditya <adityamishra2217@gmail.com>
2025-04-20 15:50:32 +0530

    buildSrc: for shared logic

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,25 @@
+
+object Versions {
+    const val gatlingVersion = "3.9.2.1"
+    const val shadowVersion = "8.1.1"
+    const val springBootVersion = "3.3.3"
+    const val springDependencyManagementVersion = "1.1.6"
+    const val benMenesVersion = "0.46.0"
+    const val kotlinVersion = "1.9.25"
+    const val sentryVersion = "4.11.0"
+
+    const val springSecurityVersion = "6.0.5"
+    const val springOauth2AutoconfigureVersion = "2.6.8"
+    const val springOauth2Version = "2.5.2.RELEASE"
+    const val hibernateValidatorVersion = "8.0.0.Final"
+    const val springDocVersion = "2.2.0"
+    const val lombokVersion = "1.18.26"
+    const val springVersion = "6.0.6"
+    const val radarSpringAuthVersion = "1.2.1"
+    const val jacksonKotlinVersion = "2.15.4"
+    const val ktorVersion = "2.3.0"
+    const val coroutinesVersion = "1.10.1"
+    const val mockitoKotlinVersion = "3.2.0"
+    const val minioVersion = "8.5.10"
+    const val junit5Version = "5.9.2"
+}
