8999bd037c5984a6386940f1df791f6375f0b386
this-Aditya <adityamishra2217@gmail.com>
2025-05-18 16:14:14 +0530

    Added base64 deserializer for jackson deserialization

---
+++ appserver-jersey/src/main/kotlin/org/radarbase/appserver/jersey/utils/Base64Deserializer.kt
@@ -0,0 +1,49 @@
+package org.radarbase.appserver.jersey.utils
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.BeanProperty
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer
+import com.fasterxml.jackson.databind.exc.InvalidFormatException
+import java.util.Base64
+
+class Base64Deserializer : JsonDeserializer<String>(), ContextualDeserializer {
+    override fun createContextual(
+        context: DeserializationContext,
+        property: BeanProperty,
+    ): JsonDeserializer<*> {
+        if (!String::class.java.isAssignableFrom(property.type.rawClass)) {
+            throw context.invalidTypeIdException(
+                property.type,
+                "String",
+                "Base64 decoding is only applied to String fields.",
+            )
+        }
+        return this
+    }
+
+    override fun deserialize(parser: JsonParser, context: DeserializationContext): String {
+        val value = clean(parser.valueAsString)
+        val decoder = Base64.getDecoder()
+
+        return try {
+            val decodedValue = decoder.decode(value)
+            String(decodedValue)
+        } catch (_: IllegalArgumentException) {
+            val fieldName = parser.parsingContext.currentName
+            val wrapperClass = parser.parsingContext.currentValue.javaClass
+
+            throw InvalidFormatException(
+                parser,
+                "Value for '$fieldName' is not a base64 encoded JSON",
+                value,
+                wrapperClass,
+            )
+        }
+    }
+
+    private fun clean(value: String): String {
+        return value.replace(Regex("[\n\r]"), "")
+    }
+}
