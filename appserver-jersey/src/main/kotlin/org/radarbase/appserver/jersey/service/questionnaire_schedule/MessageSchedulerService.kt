d58a0d5ec4dcd49f87bd9bf57a7b6646dc2ee568
this-Aditya <adityamishra2217@gmail.com>
2025-08-01 19:08:29 +0530

    Fine tuned message scheduler service as per jersey quartz

--- appserver-jersey/src/main/kotlin/org/radarbase/appserver/jersey/service/questionnaire_schedule/MessageSchedulerService.kt
+++ appserver-jersey/src/main/kotlin/org/radarbase/appserver/jersey/service/questionnaire_schedule/MessageSchedulerService.kt
@@ -27,6 +27,7 @@ import org.quartz.TriggerKey
 import org.radarbase.appserver.jersey.entity.DataMessage
 import org.radarbase.appserver.jersey.entity.Message
 import org.radarbase.appserver.jersey.entity.Notification
+import org.radarbase.appserver.jersey.entity.User
 import org.radarbase.appserver.jersey.fcm.downstream.FcmSender
 import org.radarbase.appserver.jersey.service.quartz.MessageJob
 import org.radarbase.appserver.jersey.service.quartz.MessageType
@@ -34,6 +35,7 @@ import org.radarbase.appserver.jersey.service.quartz.QuartzNamingStrategy
 import org.radarbase.appserver.jersey.service.quartz.SchedulerService
 import org.radarbase.appserver.jersey.service.quartz.SimpleQuartzNamingStrategy
 import org.slf4j.LoggerFactory
+import java.time.Instant
 import java.util.Date
 
 class MessageSchedulerService<T : Message>(
@@ -41,38 +43,45 @@ class MessageSchedulerService<T : Message>(
     val schedulerService: SchedulerService,
 ) {
     fun schedule(message: T) {
+        logger.debug("Scheduling message with id {}", message.id)
         val jobDetail = getJobDetailForMessage(message, getMessageType(message))
         if (schedulerService.checkJobExists(jobDetail.key)) {
-            println("Job has been scheduled already.")
+            logger.info("Job with key {} has been scheduled already", { jobDetail.key })
         } else {
+            logger.debug("Job Detail = {}", jobDetail);
             val trigger = getTriggerForMessage(message, jobDetail)
             schedulerService.scheduleJob(jobDetail, trigger)
         }
     }
 
     fun scheduleMultiple(messages: List<T>) {
-        val jobDetailSetMap = mutableMapOf<JobDetail, Set<Trigger>>()
-        for (message in messages) {
-            val jobDetail = getJobDetailForMessage(message, getMessageType(message))
+        val jobDetailSetMap: Map<JobDetail, Set<Trigger>> = buildMap {
+            for (message in messages) {
+                val jobDetail = getJobDetailForMessage(message, getMessageType(message))
 
-            if (schedulerService.checkJobExists(jobDetail.key)) {
-                continue
+                if (schedulerService.checkJobExists(jobDetail.key)) {
+                    logger.info("Job with key {} is already scheduled", { jobDetail.key })
+                    continue
+                }
+
+                setOf(getTriggerForMessage(message, jobDetail)).also { triggers ->
+                    this.putIfAbsent(jobDetail, triggers)
+                }
             }
-            val triggerSet = setOf(getTriggerForMessage(message, jobDetail))
-            jobDetailSetMap[jobDetail] = triggerSet
         }
-        schedulerService.scheduleJobs(jobDetailSetMap.toMap())
+        logger.info("Scheduling {} messages", jobDetailSetMap.size)
+        schedulerService.scheduleJobs(jobDetailSetMap)
     }
 
     fun updateScheduled(message: T) {
-        val jobKeyString: String =
-            NAMING_STRATEGY.getJobKeyName(
-                message.user!!.subjectId!!, message.id.toString(),
-            )
+        val (messageId: Long, subjectId: String) = nonNullMessageIdAndSubjectId(message)
+        val jobKeyString: String = NAMING_STRATEGY.getJobKeyName(
+            subjectId, messageId.toString(),
+        )
         val jobKey = JobKey(jobKeyString)
         val triggerKeyString: String =
             NAMING_STRATEGY.getTriggerName(
-                message.user!!.subjectId!!, message.id.toString(),
+                subjectId, messageId.toString(),
             )
         val triggerKey = TriggerKey(triggerKeyString)
         val jobDataMap = JobDataMap()
@@ -81,16 +90,16 @@ class MessageSchedulerService<T : Message>(
     }
 
     fun deleteScheduledMultiple(messages: List<T>) {
-        val keys = messages.map {
-            JobKey(NAMING_STRATEGY.getJobKeyName(it.user!!.subjectId!!, it.id.toString()))
+        messages.map {
+            val (messageId: Long, subjectId: String) = nonNullMessageIdAndSubjectId(it)
+            JobKey(NAMING_STRATEGY.getJobKeyName(subjectId, messageId.toString()))
         }
-        schedulerService.deleteScheduledJobs(keys)
+            .let(schedulerService::deleteScheduledJobs)
     }
 
-
     fun deleteScheduled(message: T) {
-        val key = JobKey(NAMING_STRATEGY.getJobKeyName(message.user!!.subjectId!!, message.id.toString()))
-        schedulerService.deleteScheduledJob(key)
+        JobKey(NAMING_STRATEGY.getJobKeyName(message.user!!.subjectId!!, message.id.toString()))
+            .let(schedulerService::deleteScheduledJob)
     }
 
     fun getMessageType(message: T): MessageType = when (message) {
@@ -100,27 +109,32 @@ class MessageSchedulerService<T : Message>(
     }
 
     companion object {
+        private val logger = LoggerFactory.getLogger(MessageSchedulerService::class.java)
 
-        private val log = LoggerFactory.getLogger(MessageSchedulerService::class.java)
-
-        // TODO add a schedule cache to cache incoming requests
         val NAMING_STRATEGY: QuartzNamingStrategy = SimpleQuartzNamingStrategy()
 
+        /**
+         * Build a Quartz [Trigger] that will fire exactly once at the message’s scheduled time.
+         *
+         * @param message      the [Message] whose scheduling fields must be non-null
+         * @param jobDetail    the [JobDetail] to which this trigger will be bound
+         * @return a one-shot [Trigger] that starts at `message.scheduledTime`
+         * @throws IllegalArgumentException if any of `message.id`, `message.user?.subjectId` or
+         *                                  `message.scheduledTime` is null
+         */
         fun getTriggerForMessage(message: Message, jobDetail: JobDetail): Trigger {
-            val triggerKey = TriggerKey(
-                NAMING_STRATEGY.getTriggerName(
-                    message.user!!.subjectId!!, message.id.toString(),
-                ),
-            )
+            val (messageId: Long, subjectId: String, scheduledTime: Instant) = nonNullTriggerUtils(message)
 
             return TriggerBuilder.newTrigger()
-                .withIdentity(triggerKey)
-                .forJob(
-                    NAMING_STRATEGY.getJobKeyName(
-                        message.user!!.subjectId!!, message.id.toString(),
+                .withIdentity(
+                    TriggerKey(
+                        NAMING_STRATEGY.getTriggerName(
+                            subjectId, messageId.toString(),
+                        ),
                     ),
                 )
-                .startAt(Date(message.scheduledTime!!.toEpochMilli()))
+                .forJob(jobDetail)
+                .startAt(Date(scheduledTime.toEpochMilli()))
                 .withSchedule(
                     SimpleScheduleBuilder.simpleSchedule()
                         .withRepeatCount(0)
@@ -130,29 +144,87 @@ class MessageSchedulerService<T : Message>(
                 .build()
         }
 
+        /**
+         * Build a Quartz [JobDetail] that carries the message payload.
+         *
+         * @param message      the [Message] whose fields must be non-null
+         * @param messageType  the type of the message
+         * @return a durable [JobDetail] with its [JobDataMap] populated from `message` and `messageType`
+         * @throws IllegalArgumentException if any of `message.id`, `message.user?.subjectId`,
+         *                                  `message.user?.project?.projectId` is null
+         */
         fun getJobDetailForMessage(message: Message, messageType: MessageType): JobDetail {
-            val jobKey = JobKey(
-                NAMING_STRATEGY.getJobKeyName(
-                    message.user!!.subjectId!!, message.id.toString(),
-                ),
-            )
+            val (messageId: Long, subjectId: String, projectId: String) = nonNullJobUtils(message)
 
             val dataMap = JobDataMap(
                 mapOf(
-                    "subjectId" to message.user!!.subjectId,
-                    "projectId" to message.user!!.project!!.projectId,
-                    "messageId" to message.id,
+                    "subjectId" to subjectId,
+                    "projectId" to projectId,
+                    "messageId" to messageId,
                     "messageType" to messageType.toString(),
                 ),
             )
 
             return JobBuilder.newJob(MessageJob::class.java)
-                .withIdentity(jobKey)
+                .withIdentity(
+                    JobKey(
+                        NAMING_STRATEGY.getJobKeyName(
+                            subjectId, messageId.toString(),
+                        ),
+                    ),
+                )
                 .withDescription("Send message at scheduled time...")
                 .setJobData(dataMap)
-                .storeDurably(true) // equivalent to setDurability(true)
+                .storeDurably(true)
                 .build()
         }
+
+        /**
+         * Extract and validate the three mandatory scheduling fields from [message].
+         *
+         * @return a [Triple] of `(messageId, subjectId, scheduledTime)`
+         * @throws IllegalArgumentException if any required field is null
+         */
+        fun nonNullTriggerUtils(message: Message): Triple<Long, String, Instant> {
+            val (messageId: Long, subjectId: String) = nonNullMessageIdAndSubjectId(message)
+            val scheduledTime: Instant = requireNotNull(message.scheduledTime) { "Scheduled time cannot be null" }
+
+            return Triple(messageId, subjectId, scheduledTime)
+        }
+
+        /**
+         * Extract and validate the three mandatory job-creation fields from [message].
+         *
+         * @return a [Triple] of `(messageId, subjectId, projectId)`
+         * @throws IllegalArgumentException if any required field is null
+         */
+        fun nonNullJobUtils(message: Message): Triple<Long, String, String> {
+            val (messageId: Long, subjectId: String) = nonNullMessageIdAndSubjectId(message)
+
+            val user: User = requireNotNull(message.user) { "User for message cannot be null" }
+            val projectId: String = requireNotNull(
+                requireNotNull(user.project) { "Project for user in message cannot be null" }
+                    .projectId,
+            ) { "Project Id for user in message cannot be null" }
+
+            return Triple(messageId, subjectId, projectId)
+        }
+
+        /**
+         * Extract and validate the two mandatory common fields from [message].
+         *
+         * @return a [Pair] of `(messageId, subjectId)`
+         * @throws IllegalArgumentException if `message.id` or `message.user?.subjectId` is null
+         */
+        fun nonNullMessageIdAndSubjectId(message: Message): Pair<Long, String> {
+            val messageId: Long = requireNotNull(message.id) { "Message Id cannot be null" }
+            val subjectId: String = requireNotNull(
+                requireNotNull(message.user) { "User for message cannot be null" }.subjectId,
+            ) { "Subject Id in message cannot be null" }
+
+            return Pair(messageId, subjectId)
+        }
+
     }
 }
 
