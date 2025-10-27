cf0e19cedb4b0221124c17ab231d73bcd1213208
this-Aditya <adityamishra2217@gmail.com>
2025-10-27 17:45:33 +0530

    Added application class for project microservice

---
+++ microservices/project-service/src/main/kotlin/org/radarbase/appserver/microservices/project/ProjectServiceApplication.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025 King's College London
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.radarbase.appserver.microservices.project
+
+import org.radarbase.appserver.microservices.project.config.ProjectServiceConfig
+import org.radarbase.jersey.GrizzlyServer
+import org.radarbase.jersey.config.ConfigLoader
+import org.slf4j.LoggerFactory
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    val logger = LoggerFactory.getLogger("org.radarbase.appserver.microservices.project.ProjectService")
+
+    logger.info("Starting Project Service")
+
+    val config = try {
+        ConfigLoader.loadConfig<ProjectServiceConfig>(
+            listOf(
+                "microservices/project-service/src/main/resources/project-service.yml",
+                "/etc/project-service/project-service.yml",
+            ),
+            args,
+        )
+    } catch (_: IllegalArgumentException) {
+        logger.info("No configuration file (project-service.yml) found. Exiting...")
+        exitProcess(1)
+    }
+
+    ConfigLoader.loadResources(config.resourceConfig, config).run {
+        GrizzlyServer(config.server.baseUri, this)
+    }.let { server: GrizzlyServer ->
+        server.start()
+    }
+}
