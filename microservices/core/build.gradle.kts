c8e6290aa470d6bbbc0d7b0acbfcad155abb021c
this-Aditya <adityamishra2217@gmail.com>
2025-10-27 15:36:01 +0530

    Setting up build and readme for microservices core

---
+++ microservices/core/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("plugin.allopen")
+    kotlin("plugin.noarg")
+}
+
+description = "Core library containing shared configurations, utilities, and common data models required by appserver microservices."
+
+allOpen {
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+}
+
+dependencies {
+    api("org.radarbase:radar-jersey-hibernate:${Versions.radarJerseyVersion}") {
+        runtimeOnly("org.postgresql:postgresql:${Versions.postgresqlVersion}")
+    }
+    implementation("com.google.firebase:firebase-admin:9.3.0") {
+        constraints {
+            implementation("com.google.protobuf:protobuf-java:3.25.5") {
+                because("Provided version of protobuf has security vulnerabilities")
+            }
+            implementation("com.google.protobuf:protobuf-java-util:3.25.5") {
+                because("Provided version of protobuf has security vulnerabilities")
+            }
+        }
+    }
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+}
+
+allOpen {
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+radarKotlin {
+    javaVersion.set(Versions.java)
+    kotlinVersion.set(Versions.kotlinVersion)
+//    kotlinApiVersion.set(Versions.kotlinVersion)
+    junitVersion.set(Versions.junit5Version)
+    log4j2Version.set(Versions.log4j2)
+}
