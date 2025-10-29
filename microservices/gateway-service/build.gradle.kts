8487354686bd31484f717a7cd0ff14f1894d3e40
this-Aditya <adityamishra2217@gmail.com>
2025-10-29 16:51:04 +0530

    Updated build for gateway service

--- microservices/gateway-service/build.gradle.kts
+++ microservices/gateway-service/build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
 
 dependencies {
     implementation(project(":microservices:core"))
+    implementation(project(":microservices:contract"))
     implementation("org.radarbase:radar-jersey:${Versions.radarJerseyVersion}")
 }
