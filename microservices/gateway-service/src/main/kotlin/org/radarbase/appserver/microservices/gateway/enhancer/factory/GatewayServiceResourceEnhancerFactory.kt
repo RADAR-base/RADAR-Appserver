c50a9e2ea1e4fb359a1b0ffeb03c9f7a54049433
this-Aditya <adityamishra2217@gmail.com>
2025-10-29 13:27:51 +0530

    Created gateway resource enhancer factory

---
+++ microservices/gateway-service/src/main/kotlin/org/radarbase/appserver/microservices/gateway/enhancer/factory/GatewayServiceResourceEnhancerFactory.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025 King's College London
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.radarbase.appserver.microservices.gateway.enhancer.factory
+
+import org.radarbase.appserver.microservices.gateway.config.GatewayConfig
+import org.radarbase.appserver.microservices.gateway.enhancer.GatewayServiceResourceEnhancer
+import org.radarbase.jersey.auth.AuthConfig
+import org.radarbase.jersey.auth.MPConfig
+import org.radarbase.jersey.enhancer.EnhancerFactory
+import org.radarbase.jersey.enhancer.Enhancers
+import org.radarbase.jersey.enhancer.JerseyResourceEnhancer
+
+class GatewayServiceResourceEnhancerFactory(
+    private val config: GatewayConfig,
+) : EnhancerFactory {
+    override fun createEnhancers(): List<JerseyResourceEnhancer> {
+        val authConfig = AuthConfig(
+            managementPortal = MPConfig(
+                url = config.auth.managementPortalUrl,
+            ),
+            jwtResourceName = config.auth.resourceName,
+            jwtIssuer = config.auth.issuer,
+            jwksUrls = config.auth.publicKeyUrls ?: emptyList(),
+        )
+
+        return listOf(
+            GatewayServiceResourceEnhancer(config),
+            Enhancers.radar(authConfig),
+            Enhancers.managementPortal(authConfig),
+            Enhancers.health,
+            Enhancers.exception,
+        )
+    }
+}
