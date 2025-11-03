2875c11b6b8e625d841e2fbe66acb109c88fe7e9
this-Aditya <adityamishra2217@gmail.com>
2025-11-03 14:59:32 +0530

    Resource enhancers and factory added for task state event service

---
+++ microservices/task-state-event-service/src/main/kotlin/org/radarbase/appserver/microservices/task_state_event/enhancer/TaskStateEventServiceResourceEnhancer.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2025 King's College London
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.radarbase.appserver.microservices.task_state_event.enhancer
+
+import jakarta.inject.Singleton
+import org.glassfish.jersey.internal.inject.AbstractBinder
+import org.glassfish.jersey.server.ResourceConfig
+import org.glassfish.jersey.server.validation.ValidationFeature
+import org.radarbase.appserver.microservices.core.exception.handler.UnhandledExceptionMapper
+import org.radarbase.appserver.microservices.task_state_event.config.TaskStateEventServiceConfig
+import org.radarbase.jersey.enhancer.JerseyResourceEnhancer
+import org.radarbase.jersey.service.AsyncCoroutineService
+import org.radarbase.jersey.service.ScopedAsyncCoroutineService
+
+class TaskStateEventServiceResourceEnhancer(private val config: TaskStateEventServiceConfig) : JerseyResourceEnhancer {
+    override val packages: Array<String>
+        get() = arrayOf(
+            "org.radarbase.appserver.microservices.task_state_event.api",
+        )
+
+    override fun AbstractBinder.enhance() {
+        bind(config)
+            .to(TaskStateEventServiceConfig::class.java)
+            .`in`(Singleton::class.java)
+
+        bind(ScopedAsyncCoroutineService::class.java)
+            .to(AsyncCoroutineService::class.java)
+            .`in`(Singleton::class.java)
+
+    }
+
+    override fun ResourceConfig.enhance() {
+        register(ValidationFeature::class.java)
+        register(UnhandledExceptionMapper::class.java)
+    }
+
+}
---
+++ microservices/task-state-event-service/src/main/kotlin/org/radarbase/appserver/microservices/task_state_event/enhancer/factory/TaskStateEventServiceResourceEnhancerFactory.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2025 King's College London
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.radarbase.appserver.microservices.task_state_event.enhancer.factory
+
+import org.radarbase.appserver.microservices.task_state_event.config.TaskStateEventServiceConfig
+import org.radarbase.appserver.microservices.task_state_event.enhancer.TaskStateEventServiceResourceEnhancer
+import org.radarbase.jersey.enhancer.EnhancerFactory
+import org.radarbase.jersey.enhancer.Enhancers
+import org.radarbase.jersey.enhancer.JerseyResourceEnhancer
+import org.radarbase.jersey.hibernate.config.DatabaseConfig
+import org.radarbase.jersey.hibernate.config.HibernateResourceEnhancer
+
+class TaskStateEventServiceResourceEnhancerFactory(private val config: TaskStateEventServiceConfig) : EnhancerFactory {
+    override fun createEnhancers(): List<JerseyResourceEnhancer> {
+        val dbConfig = DatabaseConfig(
+            managedClasses = config.db.classes,
+            url = config.db.jdbcUrl,
+            driver = config.db.jdbcDriver,
+            user = config.db.username,
+            password = config.db.password,
+            dialect = config.db.hibernateDialect,
+            properties = config.db.additionalProperties,
+            liquibase = org.radarbase.jersey.hibernate.config.LiquibaseConfig(
+                enable = config.db.liquibase.enabled,
+            ),
+        )
+
+        return listOf(
+            TaskStateEventServiceResourceEnhancer(config),
+            HibernateResourceEnhancer(dbConfig),
+            Enhancers.health,
+            Enhancers.exception,
+        )
+    }
+}
