37cce83e382193819a16a86cfb10cd36bdae9d79
this-Aditya <adityamishra2217@gmail.com>
2025-11-04 20:55:17 +0530

    Base setup for task service

---
+++ microservices/task-service/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    application
+}
+
+description = "AppServer Task Microservices Implementation"
+
+dependencies {
+    implementation("com.h2database:h2:${Versions.h2Version}")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    implementation("org.radarbase:radar-jersey:${Versions.radarJerseyVersion}")
+    implementation(project(":microservices:core"))
+    implementation(project(":microservices:contract"))
+}
