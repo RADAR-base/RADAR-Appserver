8e4d2a96959d6039d18f6edee9cd93b3c93333a9
this-Aditya <adityamishra2217@gmail.com>
2025-11-09 17:12:27 +0530

    Work on task state event microservice

--- appserver-jersey/src/main/kotlin/org/radarbase/appserver/jersey/service/TaskStateEventService.kt
+++ appserver-jersey/src/main/kotlin/org/radarbase/appserver/jersey/service/TaskStateEventService.kt
@@ -110,8 +110,8 @@ class TaskStateEventService @Inject constructor(
                     MapSerializer(String.serializer(), String.serializer()),
                     taskStateEventDto.associatedInfo!!,
                 )
-            } catch (exc: IOException) {
-                throw IllegalStateException(
+            } catch (exc: IllegalArgumentException) {
+                throw IllegalArgumentException(
                     "Cannot convert additionalInfo to Map<String, String>. Please check its format.",
                     exc,
                 )
