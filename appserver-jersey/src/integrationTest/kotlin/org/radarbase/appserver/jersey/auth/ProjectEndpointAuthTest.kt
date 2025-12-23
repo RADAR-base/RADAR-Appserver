6d0906d05b556db69501dc642147a81b1eff9159
this-Aditya <adityamishra2217@gmail.com>
2025-12-23 14:18:55 +0000

    Misc changes

--- appserver-jersey/src/integrationTest/kotlin/org/radarbase/appserver/jersey/auth/ProjectEndpointAuthTest.kt
+++ appserver-jersey/src/integrationTest/kotlin/org/radarbase/appserver/jersey/auth/ProjectEndpointAuthTest.kt
@@ -41,7 +41,6 @@ import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
 import org.radarbase.appserver.jersey.auth.commons.MpOAuthSupport
-import org.radarbase.appserver.jersey.auth.testSupport.IntegrationTestBase
 import org.radarbase.appserver.jersey.dto.ProjectDto
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
